07eeaf85927f2920fedb05b28d2e702daff34212
federico arias <fede.ariass33@gmail.com>
2022-10-13 18:02:22 -0300

    v2 adding % excellent adult cat x15

--- codigo.js
+++ codigo.js
@@ -1,21 +1,67 @@
+var modo = prompt('PRECIO NORMAL (N)  -  PRECIO EXCELLENT (V)');
 var name = prompt(`NOMBRE DE LA BOLSA`);
 var peso = prompt(`CUÁNTO PESA ${name}?`);
 var precio = prompt(`PRECIO DE ${name}`);
 // parseInt
 var precioP = parseInt(precio);
 
-const bolsa = (a)=>{
-    let porcentaje = (a * 25) / 100;
-    let res = porcentaje + a;
-    document.write(`<H2><span style = 'color:red'>${name}</span> BOLSA <span style = 'color:green'>$ ${res}</span></H2>`);
+
+if (modo == 'n' || modo == 'N'){
+    const bolsa = (a)=>{
+        let porcentaje = ( a* 25) / 100;
+        let res = porcentaje + a;
+        document.write(`<H2><span style = 'color:red'>${name}</span> BOLSA <span style = 'color:green'>$ ${res}</span></H2>`);
+    }
+    
+    const kilo = (a,b)=>{
+        let porcentaje = a * 40 / 100;
+        let suma = porcentaje + a;
+        let res = suma / b;
+        document.write(`<H2><span style = 'color:red'>${name}</span> KILO <span style = 'color:green'>$ ${res}</span></H2>`);
+    }
+    
+    bolsa(precioP);
+    kilo(precioP,peso);
+}else if(modo == 'v' || modo == 'V'){
+
+    const veinte = (a)=>{
+        let porcentaje = (a * 20) / 100;
+        let res = porcentaje + a;
+        document.write(`<H2><span style = 'color:red'>${name}</span> BOLSA <span style = 'color:green'>$ ${res}</span></H2>`);
+    }
+
+    const kiloV = (a, b)=>{
+        let porcentaje = a * 20 / 100;
+        let suma = porcentaje + a;
+        let res = suma / b;
+        document.write(`<H2><span style = 'color:red'>${name}</span> BOLSA <span style = 'color:green'>$ ${res}</span></H2>`);
+    }
+
+    veinte(precioP);
+    kiloV(precioP, peso);
+
+
 }
 
-const kilo = (a,b)=>{
-    let porcentaje = a * 40 / 100;
-    let suma = porcentaje + a;
-    let res = suma / b;
-    document.write(`<H2><span style = 'color:red'>${name}</span> KILO <span style = 'color:green'>$ ${res}</span></H2>`);
-}
 
-bolsa(precioP);
-kilo(precioP,peso);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
